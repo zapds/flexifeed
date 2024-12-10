4cbd1a3f5b60f36262c40565277a4f2d237e1e1b
zapds <71247896+zapds@users.noreply.github.com>
2024-12-10 14:05:26 +0530

    add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,31 @@
 <script>
 	import '../app.css';
-	let { children } = $props();
+	let { children, data } = $props();
+    import { Button } from "$lib/components/ui/button";
 </script>
+
+<nav class="flex flex-row w-full items-center gap-4">
+	<Button variant="ghost" href="/">
+		FlexiFeed
+	</Button>
+
+	<Button variant="ghost" href="/protected">
+		Protected
+	</Button>
+
+	<Button variant="ghost" href="/about">
+		About
+	</Button>
+
+	{#if data.user}
+		<Button variant="ghost" class=" ml-auto" href="/logout">
+			Logout
+		</Button>
+	{:else}
+		<Button class=" ml-auto" href="/login/google">
+			Sign in
+		</Button>
+	{/if}
+</nav>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
     import { Button } from "$lib/components/ui/button";
-
-    let count = $state(0);
-
+    
+    let { data } = $props();
 
 </script>
 
-<h1>FlexiFeed</h1>
+<h1 class="text-8xl font-bold text-center">FlexiFeed</h1>
+{#if data.user}
+    <h2 class="text-4xl font-semibold text-center">Welcome, {data.user.name}</h2>
+{/if}
