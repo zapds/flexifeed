bf4a58b310fb537ac7449e435abb20379a4ac6e6
zapds <71247896+zapds@users.noreply.github.com>
2024-12-27 22:57:22 +0530

    rename routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
     <h1 class="text-8xl font-bold text-center mt-48 mb-2">FlexiFeed</h1>
     <h2 class="text-3xl font-semibold text-center">Personalized news,<br><span class="text-transparent bg-gradient-to-r from-[#007cf0] to-[#00dfd8] bg-clip-text">crafted just for You.</span></h2>
     {#if data.user}
-        <Button class="my-4 inline-flex gap-2" href="/feed">
+        <Button class="my-4 inline-flex gap-2" href="/genfeed">
             <Newspaper />
             <span class="font-semibold">Get your feed</span>
         </Button>
